e7d85c89d19636e46474feff3a2db486fd3d1b52
Jeffrey Wu <xr-hui@qq.com>
2026-03-02 11:24:32 +0800

    feat(pages): add markdown rendering for messages

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import '../service/api/models/session.dart';
 import '../service/api/models/message.dart';
@@ -235,9 +236,21 @@ class _MessagePart extends StatelessWidget {
       final textPart = part as TextPart;
       return Padding(
         padding: const EdgeInsets.only(top: 4),
-        child: SelectableText(
-          textPart.text,
-          style: const TextStyle(fontSize: 14, height: 1.5),
+        child: MarkdownBody(
+          data: textPart.text,
+          selectable: true,
+          styleSheet: MarkdownStyleSheet(
+            p: const TextStyle(fontSize: 14, height: 1.5),
+            code: TextStyle(
+              backgroundColor: Colors.grey[200],
+              fontFamily: 'monospace',
+              fontSize: 13,
+            ),
+            codeblockDecoration: BoxDecoration(
+              color: Colors.grey[100],
+              borderRadius: BorderRadius.circular(4),
+            ),
+          ),
         ),
       );
     } else if (part is ToolPart) {
